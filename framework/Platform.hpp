e06516fd707e36001e589580ec829fa6b0a8f0bc
OrangeCat <no>
2020-11-19 22:43:39 +0100

    add: macOS support

--- framework/Platform.hpp
+++ framework/Platform.hpp
@@ -2,6 +2,8 @@
 
 #if defined _WIN32 || defined _WIN64
 #define OS_WINDOWS
+#elif defined __APPLE__
+#define OS_MACOS
 #else
 #define OS_LINUX
 #define OS_OTHER
--- framework/SharedLibrary.hpp
+++ framework/SharedLibrary.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#include "Platform.hpp"
+
 #if defined SHARED_LIBRARY_EXPORT
+
+#if defined OS_WINDOWS
 #define shared __declspec(dllexport)
+#elif defined OS_MACOS || defined OS_LINUX
+#define shared __attribute__((visibility("default")))
+#endif
+
 #elif defined SHARED_LIBRARY_IMPORT
+
+#if defined OS_WINDOWS
 #define shared __declspec(dllimport)
 #else
 #define shared
+#endif
+
+#else
+
+#define shared
 #warning No library type defined, potentially unwanted behavior
+
 #endif
