215ccbd07d71b538662aedde647b9ceb37bfdfd5
OrangeCat <no>
2020-11-22 22:56:54 +0100

    add: algorithm to find best queues for exclusive swapchain, disabled flag for macOS padding

--- framework/Renderer/Vulkan/QueueFamilies.cpp
+++ framework/Renderer/Vulkan/QueueFamilies.cpp
@@ -4,9 +4,9 @@ Renderer::Vulkan::QueueFamilies::QueueFamilies(vk::PhysicalDevice &physicalDevic
   uint32_t i{0};
   auto     queueFamilyProperties{physicalDevice.getQueueFamilyProperties()};
   for (auto const &queueFamilyProperty : queueFamilyProperties) {
-    if (queueFamilyProperty.queueFlags & vk::QueueFlagBits::eGraphics)
+    if (queueFamilyProperty.queueFlags & vk::QueueFlagBits::eGraphics && (!graphicsFamily.has_value() || (presentFamily.has_value() && i == presentFamily.value())))
       graphicsFamily = i;
-    if (physicalDevice.getSurfaceSupportKHR(i, surface))
+    if (physicalDevice.getSurfaceSupportKHR(i, surface) && (!presentFamily.has_value() || (graphicsFamily.has_value() && i == graphicsFamily.value())))
       presentFamily = i;
     ++i;
   }
